cb94b682bfad31a33a696091f9f5df2d32e10a1f
Andrey <A.com>
2024-03-15 18:40:00 +0300

    Itog

---
+++ Main.java
@@ -0,0 +1,25 @@
+package picnic;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+public class Main {
+    static Basket basket;
+    static final String FILENAME = "picnic/input.txt";
+    static List<String> fruits;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        init();
+
+        basket.fruitsCounter(FILENAME);
+        fruits = basket.getFruits();
+
+        basket.longestFruit(FILENAME);
+
+        basket.fruitsTop(FILENAME);
+    }
+
+    private static void init() {
+        basket = new Basket();
+    }
+}
